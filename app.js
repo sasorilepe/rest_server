d2fb291184af709508204898112366e23ca6ead4
Alfredo Alvarado Lepe <alfredo.alvarado@ksquareinc.com>
2019-09-12 09:22:55 -0500

    Bug of CORS fixed

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 const userRouter = require('./routes/v1/UserRouter');
+const cors = require('cors');
 
 const port = process.env.PORT || 3500;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/v1/users', userRouter);
 
